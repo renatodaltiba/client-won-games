cb041e4fca1943a438e621d408d17f16cad441da
Renato D. Schneider <renatodaltiba@gmail.com>
2022-04-08 21:25:51 -0400

    style(Button) finalizado a estilização do botão

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, Story } from '@storybook/react'
+import { AddShoppingCart } from '@styled-icons/material/AddShoppingCart'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as Meta
+
+export const Default: Story = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy Now'
+}
+
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy Now',
+  icon: <AddShoppingCart />
+}
