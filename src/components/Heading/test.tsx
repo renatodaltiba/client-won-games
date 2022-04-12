4e785b05920ec4f2df84d8aaeaababc52fd0195f
Renato D. Schneider <renatodaltiba@gmail.com>
2022-04-12 09:55:52 -0400

    test(Heading) adcionado novos testes para mudanca de cor

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -21,7 +21,7 @@ describe('<Heading />', () => {
   it('deve renderizar o componente com uma linha na lateral', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'border-left': '0.7rem solid #F231A5'
     })
   })
   it('deve renderizar o componente com uma linha em baixo', () => {
@@ -33,5 +33,48 @@ describe('<Heading />', () => {
         modifier: '::after'
       }
     )
+  })
+
+  it('deve renderizar o componente com um tamanho pequeno', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+
+  it('deve renderizar o componente com a linha com a cor primaria', () => {
+    renderWithTheme(
+      <Heading lineColor="primary" lineBottom lineLeft>
+        Lorem Ipsum
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
+  })
+
+  it('deve renderizar o componente com a linha com a cor secundaria', () => {
+    renderWithTheme(
+      <Heading lineColor="secondary" lineBottom lineLeft>
+        Lorem
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after'
+    })
   })
 })
