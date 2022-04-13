5fca682522ab2365cd0078f3e1111638d6f6494c
Renato D. Schneider <renatodaltiba@gmail.com>
2022-04-13 10:53:42 -0400

    test(highlight): finalizado os testes de estilização

---
+++ src/components/Highlight/test.tsx
@@ -0,0 +1,74 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Highlight from '.'
+import * as S from './styles'
+
+const props = {
+  title: 'Heading 1',
+  subtitle: 'Heading 2',
+  backgroundImage: '/img/red-dead-img.jpg',
+  buttonLabel: 'Buy now',
+  buttonLink: 'https://rocketseat.com.br'
+}
+
+describe('<Highlight />', () => {
+  it('deve renderizar o componente com o cabeçalho e o botão', () => {
+    renderWithTheme(<Highlight {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Heading 1/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Heading 2/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /Buy now/i })).toBeInTheDocument()
+  })
+
+  it('deve renderizar o componente com uma imagem de background', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyle({
+      backgroundImage: `url(${props.backgroundImage})`
+    })
+  })
+
+  it('deve renderizar o componente com uma imagem flutuante', () => {
+    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />)
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      '/float-image.png'
+    )
+  })
+
+  it('deve renderizar o componente alinhado a direita por padrão', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatimage content'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it('deve renderizar o componente alinhado a esquerda', () => {
+    const { container } = renderWithTheme(
+      <Highlight {...props} alignment="left" />
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatimage'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'left', {
+      modifier: `${S.Content}`
+    })
+  })
+})
