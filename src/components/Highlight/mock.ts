1036bd8db569241f12eb78023a9901f7002ee410
Renato D. Schneider <renatodaltiba@gmail.com>
2022-04-18 11:48:15 -0400

    chore: adicionado mock ao projeto

---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,7 @@
+export default {
+  title: 'Read Dead it`s back',
+  subtitle: 'The best game ever',
+  buttonLabel: 'Buy now',
+  buttonLink: 'https://rocketseat.com.br',
+  backgroundImage: '/img/red-dead-img.jpg'
+}
--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,16 +1,10 @@
 import { Meta, Story } from '@storybook/react'
 import Highlight, { HighLightProps } from '.'
-
+import item from './mock'
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'Read Dead it`s back',
-    subtitle: 'The best game ever',
-    buttonLabel: 'Buy now',
-    buttonLink: 'https://rocketseat.com.br',
-    backgroundImage: '/img/red-dead-img.jpg'
-  }
+  args: { ...item }
 } as Meta
 
 export const Default: Story<HighLightProps> = (args) => (
